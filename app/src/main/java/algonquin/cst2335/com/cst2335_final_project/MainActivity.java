97396c40e7dadd585758de6b50c57d5a1536916f
soya0002 <badal_bad123>
2018-03-30 17:09:06 -0400

    First Main Commit

---
+++ app/src/main/java/algonquin/cst2335/com/cst2335_final_project/MainActivity.java
@@ -0,0 +1,13 @@
+package algonquin.cst2335.com.cst2335_final_project;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
